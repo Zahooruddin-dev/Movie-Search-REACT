4c07bb9e82aff4ac1fc26a4ec6c5ee7c76775adc
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-09-10 21:50:11 +0500

    working now DOM too

--- movieSearch/src/App.jsx
+++ movieSearch/src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import SearchMovies from './components/searchMovies'
-import './App.css'
+import { useState } from 'react';
+import SearchMovies from './components/searchMovies';
+import './App.css';
 
 function App() {
-
-  return (
-    <div className="container">
-        <h1 className="title">React Movie Search</h1>
-        <SearchMovies/>
-      </div>
-  )
+	return (
+		<div className='container'>
+			<h1 className='title'>React Movie Search</h1>
+			<SearchMovies />
+		</div>
+	);
 }
 
-export default App
+export default App;
